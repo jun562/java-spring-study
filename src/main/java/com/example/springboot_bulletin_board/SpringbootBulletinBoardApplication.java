af6f2c27db35305a23e050e4512f9c5cafc7bf56
jun562 <capboy290@naver.com>
2025-03-17 13:32:59 +0900

    chore(file): 스프링 개발환경 구축

---
+++ src/main/java/com/example/springboot_bulletin_board/SpringbootBulletinBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_bulletin_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBulletinBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBulletinBoardApplication.class, args);
+    }
+
+}
