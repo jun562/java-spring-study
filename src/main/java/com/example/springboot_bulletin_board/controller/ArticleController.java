9480c01c5ecd7904420ed8dccc5af33fee157d6b
jun562 <capboy290@naver.com>
2025-03-24 14:09:50 +0900

    feat(read): DB 데이터 조희 추가

--- src/main/java/com/example/springboot_bulletin_board/controller/ArticleController.java
+++ src/main/java/com/example/springboot_bulletin_board/controller/ArticleController.java
@@ -5,8 +5,13 @@ import com.example.springboot_bulletin_board.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j //롬복 로깅
 @Controller
@@ -31,5 +36,24 @@ public class ArticleController {
         log.info(saved.toString());
 //        System.out.println(saved.toString()); //article -> DB 저장 확인
         return "";
+    }
+    @GetMapping("/articles/{id}") //URL에 직접 id 입력하여 요청
+    public String show(@PathVariable Long id, Model model){
+        log.info("id " + id);
+//        1. id를 조회해 DB에서 해당 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null); //Optional은 NPE을 피하기 위해 사용
+//        2. 가져온 데이터를 모델에 등록하기
+        model.addAttribute("article", articleEntity);
+//        3. 뷰 페이지 반환하기
+        return "articles/show"; //항상 상대 경로는 templates의 하위 디렉토리
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+//        1. 모든 데이터 가져오기
+        List<Article> articleEntityList =(List<Article>)articleRepository.findAll(); //findAll 반환 타입은 Iterable
+//        2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+//        3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- src/main/java/com/example/springboot_bulletin_board/repository/ArticleRepository.java
+++ src/main/java/com/example/springboot_bulletin_board/repository/ArticleRepository.java
@@ -3,7 +3,10 @@ package com.example.springboot_bulletin_board.repository;
 import com.example.springboot_bulletin_board.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article,Long> //<엔티티 클래스 타입, 엔티티 대표값(ID) 타입>
 {
-
+    @Override
+    ArrayList<Article> findAll();
 }
